9766691753bd60c0cbd1b79ae6a5ae0af44e0432
Soham-008 <157625728+Soham-008@users.noreply.github.com>
2025-07-30 19:14:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const no = document.getElementById("display")
+
+function cleardisplay(){
+    no.value = ""
+
+}
+function appendToDisplay(input){
+
+    console.log(no.value += input)
+
+}
+function calculate(){
+    console.log("You pressed on compute")
+    try{
+        no.value = eval(no.value)
+
+    }
+    catch(error){
+        no.value="ERROR"
+    }
+    
+     
+}
